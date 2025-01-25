64f2a9c42ae6f8bf6357e0c66c802dda0dbf9674
vtitov <v.v.titov94@gmail.com>
2025-01-25 20:19:37 +0300

    feat: set base _request and make get_project api call

--- src/gitlb_client/client.py
+++ src/gitlb_client/client.py
@@ -1,22 +1,73 @@
+from enum import Enum
+from pprint import pprint
+from typing import Any
+from urllib.parse import urljoin
+from . import types
 import aiohttp
 
 
+class GitlabAuthType(str, Enum):
+    header = 'header'
+    bearer = 'bearer'
+    query_params = 'query_params'
+
+
 class GitlabHTTPClient:
-    def __init__(self, url: str, token: str, session: aiohttp.ClientSession):
+    def __init__(
+        self,
+        base_url: str,
+        access_token: str,
+        session: aiohttp.ClientSession,
+        auth_type: GitlabAuthType = GitlabAuthType.header,
+    ):
         """
         Initialize a GitlabHTTPClient instance.
 
-        Args:
-            url (str): The base URL of the Gitlab API.
-            token (str): The access token for accessing the Gitlab API.
-            session (aiohttp.ClientSession): An aiohttp ClientSession object for making HTTP requests.
-        """
-        self.url = url
-        self.token = token
-        self.session = session
+        This method sets up the client with the necessary authentication and session information
+        for making requests to the GitLab API.
 
-    async def get_projects(self):
-        pass
+        Args:
+            base_url (str): The base URL of the GitLab API.
+            access_token (str): The access token for authenticating with the GitLab API.
+            session (aiohttp.ClientSession): An aiohttp ClientSession object for making HTTP requests.
+            auth_type (GitlabAuthType, optional): The type of authentication to use.
+                Defaults to GitlabAuthType.header.
+
+        Note:
+            Depending on the auth_type, this method will set up either access headers or query parameters
+            for authentication in subsequent API requests.
+        """
+
+        self.base_url = base_url
+        self.session = session
+        self.auth_type = auth_type
+
+        if auth_type == GitlabAuthType.bearer:
+            self.access_headers = {'Authorization': f'Bearer {access_token}'}
+        elif auth_type == GitlabAuthType.header:
+            self.access_headers = {'PRIVATE-TOKEN': access_token}
+        else:
+            self.access_params = {'private_token': access_token}
+
+    async def _request(
+        self, method: str, path: str, headers: dict | None = None, params: dict | None = None, *args, **kwargs,
+    ) -> Any:
+        url = urljoin(self.base_url, path)
+
+        if self.auth_type in (GitlabAuthType.bearer, GitlabAuthType.header):
+            headers = {**self.access_headers, **headers} if headers else self.access_headers
+
+        if self.auth_type == GitlabAuthType.query_params:
+            params = {**self.access_params, **params} if params else self.access_params
+
+        async with self.session.request(
+            method=method, url=url, headers=headers, params=params, *args, **kwargs,
+        ) as resp:
+            return await resp.json()
+
+    async def get_projects(self) -> types.ProjectList:
+        response = await self._request(method='GET', path='api/v4/projects')
+        return types.ProjectList.model_validate(response)
 
     async def get_project_mrs(self, project_id: int):
         pass
--- src/gitlb_client/types.py
+++ src/gitlb_client/types.py
@@ -1,38 +1,5 @@
-from typing import List, Optional
-from datetime import datetime
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, RootModel
 
 
-class Namespace(BaseModel):
-    id: int
-    name: str
-    path: str
-    kind: str
-    full_path: str
-    parent_id: Optional[int] = None
-    avatar_url: Optional[HttpUrl] = None
-    web_url: HttpUrl
-
-
-class Project(BaseModel):
-    id: int
-    description: Optional[str] = None
-    name: str
-    name_with_namespace: str
-    path: str
-    path_with_namespace: str
-    created_at: datetime
-    default_branch: str
-    tag_list: List[str]
-    topics: List[str]
-    ssh_url_to_repo: str
-    http_url_to_repo: HttpUrl
-    web_url: HttpUrl
-    avatar_url: Optional[HttpUrl] = None
-    star_count: int
-    last_activity_at: datetime
-    namespace: Namespace
-
-
-class ProjectList(BaseModel):
-    projects: List[Project]
+class ProjectList(RootModel[list[dict]]):
+    pass
